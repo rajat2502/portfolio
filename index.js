9493ee0efcccb2ddebca269a894d9aa68f3a476a
abc <rajat2502@users.noreply.github.com>
2019-05-29 21:45:22 +0530

    add background image

--- index.js
+++ index.js
@@ -11,7 +11,20 @@ $(document).ready(function(){
     });
   });
 
+  var intro = document.getElementById('intro');
 
+  var typewriter = new Typewriter(intro, {
+      loop: true
+  });
 
+  typewriter.typeString('Hello World!')
+      .pauseFor(2500)
+      .deleteAll()
+      .typeString('Strings can be removed')
+      .pauseFor(2500)
+      .deleteChars(7)
+      .typeString('<strong>altered!</strong>')
+      .pauseFor(2500)
+      .start();
 });
 
