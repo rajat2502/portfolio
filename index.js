610ad822cd9071ba380b310c0d8d33947832e8a8
abc <rajat2502@users.noreply.github.com>
2019-05-29 19:03:31 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  $('.header').height($(window).height());
+
+  $('.nav-link').on('click', function(event){
+
+    var hash = this.hash;
+    $('html, body').animate({
+      scrollTop : $(hash).offset().top
+    }, 800, function(){
+      window.location.hash = hash;
+    });
+  });
+
+
+
+});
+
