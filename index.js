c40467d2558d9fb684383a7a9ae210eafe298e6c
abc <rajat2502@users.noreply.github.com>
2019-06-09 12:33:02 +0530

    images shrinked

--- index.js
+++ index.js
@@ -11,20 +11,5 @@ $(document).ready(function(){
     });
   });
 
-  var intro = document.getElementById('intro');
-
-  var typewriter = new Typewriter(intro, {
-      loop: true
-  });
-
-  typewriter.typeString('Hello World!')
-      .pauseFor(2500)
-      .deleteAll()
-      .typeString('Strings can be removed')
-      .pauseFor(2500)
-      .deleteChars(7)
-      .typeString('<strong>altered!</strong>')
-      .pauseFor(2500)
-      .start();
 });
 
